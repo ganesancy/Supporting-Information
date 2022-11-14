1622230c2b242cf353e29e3e8c3dbfc93ac61279
ganesancy <113365168+ganesancy@users.noreply.github.com>
2022-11-14 19:31:32 +0530

    Add files via upload

---
+++ Supporting Information/ML Deployment-Amorphous/app.py
@@ -0,0 +1,24 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# Create flask app
+app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def Home():
+    return render_template("index.html")
+
+@app.route("/predict", methods = ["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction= model.predict(features)
+    return render_template("index.html", prediction_text = "The MR present in zeolite is {}".format(prediction))
+
+
+
+
+if __name__ == "__main__":
+      app.run(debug=True)
---
+++ Supporting Information/ML Deployment-Amorphous/model.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from xgboost import XGBClassifier
+import pickle
+from sklearn.model_selection import StratifiedKFold
+# Load the csv file
+df = pd.read_excel("Final-A.xlsx")
+
+X_OS = df[['Ge/Si', 'Al/T', 'OH/T', 'H2O/T', 'F/T', 'OSDA/T', "B/T", "Na2O/T", "Cl/T", "Temperature", "time", "AR", "Area", "C/N", "rpm"]]
+Y_OS= df['RC2']
+cv = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+for train_index, test_index in cv.split(X_OS, Y_OS):
+    X_Train, X_Test= X_OS.iloc[train_index], X_OS.iloc[test_index]
+    Y_Train, Y_Test= Y_OS[train_index], Y_OS[test_index]
+
+XGB = XGBClassifier(random_state=1, n_estimators=1600, learning_rate=1,
+                   alpha=0.095, max_depth=2,colsample_bytree=0.1, reg_lambda=1, min_child_weight=3)
+xg_model=XGB.fit(X_Train.values, Y_Train)
+
+pickle.dump(xg_model, open("model.pkl", "wb"))
